98f93bc0c1aa83c0d4bf2269f628ac48a1f47e54
sahyunjin <tkguswls1106@naver.com>
2024-04-07 17:39:07 +0900

    chore: 프로젝트 초기 세팅

---
+++ README.md
@@ -0,0 +1,3 @@
+## [ Backend Repo / Spring ]<br>Study for Chatting - WebSocket & Stomp & RabbitMQ
+- <strong>version</strong>&nbsp;:&nbsp;
+  Java 17, Spring Boot 3.1.10
---
+++ src/main/java/com/shj/springchatting/SpringChattingApplication.java
@@ -0,0 +1,23 @@
+package com.shj.springchatting;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.TimeZone;
+
+@EnableJpaAuditing
+@SpringBootApplication
+public class SpringChattingApplication {
+
+	@PostConstruct
+	public void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringChattingApplication.class, args);
+	}
+
+}
