f9753d2c131ddc6916ba883fa952731bb0b1d663
sahyunjin <tkguswls1106@naver.com>
2024-04-07 21:41:02 +0900

    fix: 설명 주석 수정

--- src/main/java/com/shj/springchatting/config/WebSocketConfig.java
+++ src/main/java/com/shj/springchatting/config/WebSocketConfig.java
@@ -14,8 +14,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/sub");  // 메시지 구독 url (topic을 구독)
-        config.setApplicationDestinationPrefixes("/pub");  // 메시지 발행 url
+        config.enableSimpleBroker("/sub");  // 메시지 구독 url (topic을 구독) (즉, 현재 프론트엔드의 채팅방을 의미하여, 프론트엔드가 백엔드에게 응답받을 주소임. = 프론트엔드 주소)
+        config.setApplicationDestinationPrefixes("/pub");  // 메시지 발행 url (즉, 프론트엔드가 백엔드로 요청할 주소임. = 백엔드 주소)
     }
 
     @Override
