f9c4c25f588132decd62aa9df8d4901ade72e877
sahyunjin <tkguswls1106@naver.com>
2024-04-15 19:30:04 +0900

    feat: RabbitConfig 수정 및 other 관련 추가 - nojwt Branch / 배포 가능

---
+++ src/main/java/com/shj/springchatting/dto/other/OtherRequestDto.java
@@ -0,0 +1,12 @@
+package com.shj.springchatting.dto.other;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class OtherRequestDto {
+
+    private String senderName;
+    private String data;
+}
